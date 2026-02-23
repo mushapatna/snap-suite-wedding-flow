1896834190f48083141ddbf43bfcf27dd7cc42b3
mushapatna <iqubalmushsraf@gmail.com>
2026-02-23 16:59:12 +0530

    Remove legacy supabase folder

--- supabase/migrations/20250728123859-3179b29d-a6df-4a43-88cc-4823810e7338.sql
+++
@@ -1,5 +0,0 @@
--- Fix search path for existing functions
-ALTER FUNCTION public.update_updated_at_column() SET search_path = '';
-ALTER FUNCTION public.create_default_event_checklist(uuid) SET search_path = '';
-ALTER FUNCTION public.handle_new_event() SET search_path = '';
-ALTER FUNCTION public.handle_new_user() SET search_path = '';
